f7c43974d9407a796c2087eb8d4c4a7e3c6265db
seda-karadeniz <sedayo98@gmail.com>
2021-08-18 23:34:49 +0200

    update

--- index.php
+++ index.php
@@ -1,5 +1,81 @@
 <?php
 
+define('TODAY', (new DateTime('now', new DateTimeZone('Europe/Brussels')))->format('M jS, Y')); /*M=month, j= jour sans le 0 devant S= suffix exemple th ou nd Y= year 4chiffre */
+$filePath = 'matches.csv';
+$matches = [];
+$standings = [];
+$teams = [];
+
+function getEmptyStatsArray(){
+    return [
+        'games' => 0,
+        'points' => 0,
+        'wins' => 0,
+        'losses' => 0,
+        'draws' => 0,
+        'GF' => 0,
+        'GA' => 0,
+        'GD' => 0,
+    ];
+}
+
+
+$handle = fopen($filePath,'r'); /*file open .. r=read*/
+$headers = fgetcsv($handle,1000);
+while ($line = fgetcsv($handle,1000)){
+    $match = array_combine($headers, $line); /*affecter ca au tableau comme push en js*/
+    $matches[] = $match;
+    $homeTeam = $match['home-team']; /*en gros celui qui a la clé home team */
+    $awayTeam = $match['away-team'];
+    /*dans le tableau des equipe sil ya deja la home team*/
+    /*le if en dessus -> si la home team ne fais pas partie du tableau teams -> !in_array($homeTeam, $teams)*/
+
+    if (!array_key_exists($homeTeam, $standings)){
+        $standings[$homeTeam]= getEmptystatsArray();
+    }
+    if (!array_key_exists($awayTeam, $standings)){
+        $standings[$awayTeam]= getEmptystatsArray();
+    }
+    $standings[$homeTeam]['games']++;
+    $standings[$awayTeam]['games']++;
+    /*incrementation dans la clé des jeu*/
+
+    if ($match['home-team-goals'] === $match['away-team-goals']) {
+        $standings[$homeTeam]['points']++;
+        $standings[$awayTeam]['points']++;
+        $standings[$homeTeam]['draws']++;
+        $standings[$awayTeam]['draws']++;
+    } elseif ($match['home-team-goals'] > $match['away-team-goals']) {
+        $standings[$homeTeam]['points'] += 3;
+        $standings[$homeTeam]['wins']++;
+        $standings[$awayTeam]['losses']++;
+    } else {
+        $standings[$awayTeam]['points'] += 3;
+        $standings[$awayTeam]['wins']++;
+        $standings[$homeTeam]['losses']++;
+    }
+    $standings[$homeTeam]['GF'] += $match['home-team-goals'];
+    $standings[$homeTeam]['GA'] += $match['away-team-goals'];
+    $standings[$awayTeam]['GF'] += $match['away-team-goals'];
+    $standings[$awayTeam]['GA'] += $match['home-team-goals'];
+    $standings[$homeTeam]['GD'] = $standings[$homeTeam]['GF'] - $standings[$homeTeam]['GA'];
+    $standings[$awayTeam]['GD'] = $standings[$awayTeam]['GF'] - $standings[$awayTeam]['GA'];
+}
+
+uasort($standings, function ($a, $b){
+    if ($a['points'] === $b['points']){
+        //si les points de la premiere equipe sont egale a la deuxieme
+        return 0;
+    }
+    return $a['points'] > $b['points'] ? -1: 1;
+});
+
+$teams = array_keys($standings);
+/*toutes les valeur des equipes sont dans standings alors on met tte les clé dans teams*/
+
+
+require('vue.php');
+/*
 define('TODAY', (new DateTime('now', new DateTimeZone('Europe/Brussels')))->format('M jS, Y'));
 define('FILE_PATH', 'matches.csv');
 $matches = [];
@@ -70,4 +146,4 @@ uasort($standings, function ($a, $b) {
 $teams = array_keys($standings);
 sort($teams);
 
-require('vue.php');
+require('vue.php');*/
--- vue.php
+++ vue.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -24,20 +25,21 @@
         </tr>
         </thead>
         <tbody>
-        <?php $i = 1; ?>
+        <?php $i=1; ?>
         <?php foreach ($standings as $team => $teamStats): ?>
-            <tr>
-                <td><?= $i++; ?></td>
-                <th scope="row"><?= $team ?></th>
-                <td><?= $teamStats['games']; ?></td>
-                <td><?= $teamStats['points']; ?></td>
-                <td><?= $teamStats['wins']; ?></td>
-                <td><?= $teamStats['losses']; ?></td>
-                <td><?= $teamStats['draws']; ?></td>
-                <td><?= $teamStats['GF']; ?></td>
-                <td><?= $teamStats['GA']; ?></td>
-                <td><?= $teamStats['GD']; ?></td>
-            </tr>
+        <tr>
+            <td><?= $i; ?></td>
+            <th scope="row"><?= $team; ?></th>
+            <td><?= $teamStats['games']; ?></td>
+            <td><?= $teamStats['points']; ?></td>
+            <td><?= $teamStats['wins']; ?></td>
+            <td><?= $teamStats['losses']; ?></td>
+            <td><?= $teamStats['draws']; ?></td>
+            <td><?= $teamStats['GF']; ?></td>
+            <td><?= $teamStats['GA']; ?></td>
+            <td><?= $teamStats['GD']; ?></td>
+
+        </tr>
         <?php endforeach; ?>
 
         </tbody>
@@ -45,6 +47,119 @@
 </section>
 <section>
     <h2>Games played at <?= TODAY ?></h2>
+    <table>
+        <thead>
+        <tr>
+            <th>Date</th><th>Home Team</th><th>Home Team Goals</th><th>Away Team Goals</th><th>Away Team</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach( $matches as $match):?>
+        <tr>
+            <td><?= (new DateTime($match['match-date'], new DateTimeZone('Europe/Brussels')))->format('M jS, Y') ?></td>
+            <td><?= $match['home-team'] ?></td>
+            <td><?= $match['home-team-goals'] ?></td>
+            <td><?= $match['away-team-goals'] ?></td>
+            <td><?= $match['away-team'] ?></td>
+        </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+</section>
+<section>
+    <h2>Encodage d’un nouveau match</h2>
+    <form action="manage.php" method="post">
+        <label for="match-date">Date du match</label>
+        <input type="text" id="match-date" name="match-date">
+        <br>
+        <label for="home-team">Équipe à domicile</label>
+        <select name="home-team" id="home-team">
+            <option value="arsenal">Arsenal</option>
+            <option value="chelsea">Chelsea</option>
+            <option value="liverpool">Liverpool</option>
+            <option value="manchester%20city">Manchester City</option>
+            <option value="manchester%20united">Manchester United</option>
+            <option value="tottenham">Tottenham</option>
+        </select>
+        <label for="home-team-unlisted">Équipe non listée&nbsp;?</label>
+        <input type="text" name="home-team-unlisted" id="home-team-unlisted">
+        <br>
+        <label for="home-team-goals">Goals de l’équipe à domicile</label>
+        <input type="text" id="home-team-goals" name="home-team-goals">
+        <br>
+        <label for="away-team">Équipe visiteuse</label>
+        <select name="away-team" id="away-team">
+            <option value="arsenal">Arsenal</option>
+            <option value="chelsea">Chelsea</option>
+            <option value="liverpool">Liverpool</option>
+            <option value="manchester%20city">Manchester City</option>
+            <option value="manchester%20united">Manchester United</option>
+            <option value="tottenham">Tottenham</option>
+        </select>
+        <label for="away-team-unlisted">Équipe non listée&nbsp;?</label>
+        <input type="text" name="away-team-unlisted" id="away-team-unlisted">
+        <br>
+        <label for="away-team-goals">Goals de l’équipe visiteuse</label>
+        <input type="text" id="away-team-goals" name="away-team-goals">
+        <br>
+        <input type="submit" value="Ajouter ce match">
+    </form>
+</section>
+</body>
+</html>
+
+
+
+
+
+
+<!--<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Premier League 2020</title>
+</head>
+<body>
+<h1>Premier League 2020</h1>
+<section>
+    <h2>Standings</h2>
+    <table>
+        <thead>
+        <tr>
+            <td></td>
+            <th scope="col">Team</th>
+            <th scope="col">Games</th>
+            <th scope="col">Points</th>
+            <th scope="col">Wins</th>
+            <th scope="col">Losses</th>
+            <th scope="col">Draws</th>
+            <th scope="col">GF</th>
+            <th scope="col">GA</th>
+            <th scope="col">GD</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php /*$i = 1; */?>
+        <?php /*foreach ($standings as $team => $teamStats): */?>
+            <tr>
+                <td><?/*= $i++; */?></td>
+                <th scope="row"><?/*= $team */?></th>
+                <td><?/*= $teamStats['games']; */?></td>
+                <td><?/*= $teamStats['points']; */?></td>
+                <td><?/*= $teamStats['wins']; */?></td>
+                <td><?/*= $teamStats['losses']; */?></td>
+                <td><?/*= $teamStats['draws']; */?></td>
+                <td><?/*= $teamStats['GF']; */?></td>
+                <td><?/*= $teamStats['GA']; */?></td>
+                <td><?/*= $teamStats['GD']; */?></td>
+            </tr>
+        <?php /*endforeach; */?>
+
+        </tbody>
+    </table>
+</section>
+<section>
+    <h2>Games played at <?/*= TODAY */?></h2>
     <table>
         <thead>
         <tr>
@@ -56,16 +171,16 @@
         </tr>
         </thead>
         <tbody>
-        <?php foreach ($matches as $match): ?>
+        <?php /*foreach ($matches as $match): */?>
             <tr>
-                <td><?= (new DateTime($match['match-date'],
-                        new DateTimeZone('Europe/Brussels')))->format('M l jS, Y') ?></td>
-                <td><?= $match['home-team'] ?></td>
-                <td><?= $match['home-team-goals'] ?></td>
-                <td><?= $match['away-team-goals'] ?></td>
-                <td><?= $match['away-team'] ?></td>
+                <td><?/*= (new DateTime($match['match-date'],
+                        new DateTimeZone('Europe/Brussels')))->format('M l jS, Y') */?></td>
+                <td><?/*= $match['home-team'] */?></td>
+                <td><?/*= $match['home-team-goals'] */?></td>
+                <td><?/*= $match['away-team-goals'] */?></td>
+                <td><?/*= $match['away-team'] */?></td>
             </tr>
-        <?php endforeach; ?>
+        <?php /*endforeach; */?>
         </tbody>
     </table>
 </section>
@@ -77,9 +192,9 @@
         <br>
         <label for="home-team">Équipe à domicile</label>
         <select name="home-team" id="home-team">
-            <?php foreach ($teams as $team): ?>
-                <option value="<?= $team ?>"><?= $team ?></option>
-            <?php endforeach; ?>
+            <?php /*foreach ($teams as $team): */?>
+                <option value="<?/*= $team */?>"><?/*= $team */?></option>
+            <?php /*endforeach; */?>
         </select>
         <label for="home-team-unlisted">Équipe non listée&nbsp;?</label>
         <input type="text" name="home-team-unlisted" id="home-team-unlisted">
@@ -89,9 +204,9 @@
         <br>
         <label for="away-team">Équipe visiteuse</label>
         <select name="away-team" id="away-team">
-            <?php foreach ($teams as $team): ?>
-                <option value="<?= $team ?>"><?= $team ?></option>
-            <?php endforeach; ?>
+            <?php /*foreach ($teams as $team): */?>
+                <option value="<?/*= $team */?>"><?/*= $team */?></option>
+            <?php /*endforeach; */?>
         </select>
         <label for="away-team-unlisted">Équipe non listée&nbsp;?</label>
         <input type="text" name="away-team-unlisted" id="away-team-unlisted">
@@ -106,3 +221,4 @@
 </section>
 </body>
 </html>
+-->
